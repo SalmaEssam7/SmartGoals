6a3ca5ab7296983b37d8973ff747375e08639333
SalmaEssam7 <Sees7878@gmail.com>
2026-01-10 16:31:19 +0200

    first commit

---
+++ smart-goals/src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './Sections/NavBar/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+   <NavBar/>
+      
+     
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+  //  <p>
+    // <img src={logo} className="App-logo" alt="logo" />
+    // //  <header className="App-header"></header>
+    //  </header>
+     
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
---
+++ smart-goals/src/Sections/NavBar/NavBar.jsx
@@ -0,0 +1,19 @@
+import logo from '../../Assets/logo.png'
+import './NavBar.css'
+
+function NavBar() {
+
+  return (
+ <div className="NavBar">
+    <div className="NavBar-logo">
+        <img src={logo} alt="" />
+        {/* <p>SHOPPER</p> */}
+    </div>
+      </div>     
+  
+  )   
+}
+
+export default NavBar
+
+
